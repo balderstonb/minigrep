2c7eb570a43c3f186faa2967344f2fa18168deaf
Brian Balderston <balderstonb@gmail.com>
2019-07-16 13:56:16 -0400

    use filter

--- src/lib.rs
+++ src/lib.rs
@@ -27,7 +27,7 @@ pub struct Config {
 impl Config {
     pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
         args.next();
-        
+
         let query = match args.next() {
             Some(arg) => arg,
             None => return Err("Didn't get a query string"),
@@ -49,14 +49,9 @@ impl Config {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    results
+    contents.lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
